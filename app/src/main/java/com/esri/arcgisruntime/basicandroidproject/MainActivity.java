04d123ae45ee7f9dfb847a23e5db6d9842099b00
Christian V <christian.vincent25@hotmail.com>
2020-04-05 19:55:41 -0400

    - Implemented coordinates finder for GPS (may not need this) - Created a temporary main activity page with buttons and input area for   testing - Partially implemented the API for finding the corresponding data   for a city, state provided by the user

--- app/src/main/java/com/esri/arcgisruntime/basicandroidproject/MainActivity.java
+++ app/src/main/java/com/esri/arcgisruntime/basicandroidproject/MainActivity.java
@@ -8,6 +8,9 @@ import androidx.core.content.ContextCompat;
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.esri.arcgisruntime.ArcGISRuntimeEnvironment;
@@ -16,22 +19,33 @@ import com.esri.arcgisruntime.mapping.Basemap;
 import com.esri.arcgisruntime.mapping.view.LocationDisplay;
 import com.esri.arcgisruntime.mapping.view.MapView;
 
+import android.content.Intent;
+import android.location.Location;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
 public class MainActivity extends AppCompatActivity {
 
     private MapView mMapView;
-
     private LocationDisplay mLocationDisplay;
+    FusedLocationProviderClient locationClient;
+    double latitude = 34.0270; // latitude and longitude are overwritten if location services are turned on
+    double longitude = -118.8050;
+    EditText location;
+    Button submitButton;
+    Button GPS_button;
+    String locationVerification; //test variable
 
 
     private void setupMap() {
         if (mMapView != null) {
             ArcGISRuntimeEnvironment.setLicense(getResources().getString(R.string.arcgis_license_key));
             Basemap.Type basemapType = Basemap.Type.STREETS_VECTOR;
-            double latitude = 34.0270;
-            double longitude = -118.8050;
             int levelOfDetail = 13;
             ArcGISMap map = new ArcGISMap(basemapType, latitude, longitude, levelOfDetail);
-            mMapView.setMap(map);
+            //mMapView.setMap(map);
         }
     }
 
@@ -40,8 +54,39 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mMapView = findViewById(R.id.mapView);
+        location = (EditText) findViewById(R.id.location);
+        submitButton = (Button) findViewById(R.id.submitButton);
+        GPS_button = (Button) findViewById(R.id.GPS_button);
+        locationClient = LocationServices.getFusedLocationProviderClient(this);
+
+        getLastLocation();
         setupMap();
         setupLocationDisplay();
+
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                locationVerification = location.getText().toString();
+
+                Intent launchActivity1 = new Intent(MainActivity.this, Results.class);
+                launchActivity1.putExtra("location", locationVerification);
+                startActivity(launchActivity1);
+            }
+        });
+    }
+
+    private void getLastLocation() {
+        locationClient.getLastLocation().addOnCompleteListener(
+                new OnCompleteListener<Location>() {
+                    @Override
+                    public void onComplete(Task<Location> task) {
+                        Location location = task.getResult();
+                        longitude = location.getLongitude();
+                        latitude = location.getLatitude();
+
+                    }
+                }
+        );
     }
 
     @Override
